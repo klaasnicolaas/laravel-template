999e683f6c75e55999de3a2b86033ab895b6e993
Klaas Schoute <hello@student-techlife.com>
2024-04-29 01:40:09 +0200

    Run laravel pint

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\UserRole;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
-use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
