a60912df9bb7714642a8242b43ed13924490e17a
Klaas Schoute <hello@student-techlife.com>
2024-08-31 23:26:04 +0200

    Add navigation link and middleware

---
+++ laravel/app/Http/Middleware/AuthLogViewer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserRole;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthLogViewer
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->hasRole(UserRole::ADMIN->value)) {
+            return $next($request);
+        }
+
+        abort(403, 'Forbidden to access log viewer');
+    }
+}
