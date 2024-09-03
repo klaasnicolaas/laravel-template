5a1c9fe6410ef0fc6d0776e501e4f542eb9ae89a
Klaas Schoute <hello@student-techlife.com>
2024-09-03 15:54:56 +0200

    Add middleware to access Telescope only with admin role

---
+++ laravel/app/Http/Middleware/AuthTelescope.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserRole;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthTelescope
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->hasRole(UserRole::ADMIN)) {
+            return $next($request);
+        }
+
+        abort(403, 'Forbidden to access Telescope');
+    }
+}
